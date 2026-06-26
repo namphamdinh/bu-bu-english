00bbd60bcb27d5d62d8bbfd20123a69d04a1cf8a
namphamdinh <namphamdinh@users.noreply.github.com>
2026-06-26 09:03:47 +0700

    Use Bubu image as mascot

--- src/components/Mascot.tsx
+++ src/components/Mascot.tsx
@@ -1,6 +1,6 @@
 export const Mascot = ({ message, compact = false }: { message?: string; compact?: boolean }) => (
   <div className={`mascot ${compact ? "mascot--compact" : ""}`}>
-    <img src="./assets/mascot.png" alt="Gấu Bư Bư đang học tiếng Anh" />
+    <img src="./bubu.png" alt="Bư Bư đang học tiếng Anh" />
     {message && <div className="mascot__bubble">{message}</div>}
   </div>
 );
